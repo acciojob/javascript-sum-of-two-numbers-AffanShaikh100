803bfbc1229d1b5d521ca58dadf9e1d8db49b0e8
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-17 21:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+function sum(a,b){
+	let num1 = parseInt(a); 
+	let num2 = parseInt(b);
+	let result = num1 + num2;
+
+    if(result==0 || result<0 || result>0)
+	{
+		return result;
+	}
+	else{
+		return "Invalid input. Please enter a valid number."
+	}
+}
+
+alert(sum(input1, input2));
